56a4f5bf2759427de186b3e126a637b262559443
Guy Vales <gvales@metcove.com>
2017-10-09 14:48:25 -0400

    Initial Commit

---
+++ CareDrum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareDrum
+//
+//  Created by Guy Vales on 2017-10-09.
+//  Copyright © 2017 Guy Vales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
