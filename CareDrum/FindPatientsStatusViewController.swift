516e2144afa193d9c44b71f4c8eeed0cc1b9ba45
Guy Vales <gvales@metcove.com>
2017-10-21 14:42:56 -0400

    Now include "Find Patients" demo page that displays the distance between a given main address and his or her current location.  Map showing patient's location is also displayed

--- CareDrum/FindPatientsStatusViewController.swift
+++ CareDrum/FindPatientsStatusViewController.swift
@@ -7,15 +7,78 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class FindPatientsStatusViewController: UIViewController {
+class FindPatientsStatusViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var targetOnMap: MKMapView!
+    @IBOutlet weak var targetMainAddress: UITextField!
+    @IBOutlet weak var targetAllowedDistance: UITextField!
+    @IBOutlet weak var targetDistanceFromMainAddress: UITextField!
+    @IBOutlet weak var geoWarningMessage: UITextField!
+    
+    let manager = CLLocationManager()
+    
+    let patientsomeaddress:String = "280 Albert, Ottawa, Ontario, K1P 5G8";
+    
+    let geocoder = CLGeocoder()
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude,location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        targetOnMap.setRegion(region, animated: true)
+        self.targetOnMap.showsUserLocation = true
+        
+        location.distance(from:)
+        targetDistanceFromMainAddress.text="100 meters from Home"
+        
+        var patientmainaddress: CLLocation?
+        targetMainAddress.text = patientsomeaddress
+        geoWarningMessage.text = "ALERT: Person X now beyond set distance"
+        geocoder.geocodeAddressString(patientsomeaddress) { (placemarks, error) in
+            if let placemarks = placemarks, placemarks.count > 0 {
+                patientmainaddress = placemarks.first?.location
+            }
+            if let patientmainaddress = patientmainaddress {
+                let coordinate = patientmainaddress.coordinate
+                self.targetDistanceFromMainAddress.text = "\(coordinate.latitude), \(coordinate.longitude)"
+                location.distance(from: patientmainaddress)
+                print(location.distance(from: patientmainaddress))
+                self.targetDistanceFromMainAddress.text = "\(location.distance(from: patientmainaddress) / 1000)"
+            } else {
+                self.targetDistanceFromMainAddress.text = "No Matching Location Found"
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func getTargetStatus(_ sender: Any) {
+        targetAllowedDistance.text = "2 KM"
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
